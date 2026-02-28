bbf75e3bd2683fdfc339b55d803d26bb4d943769
Shubham2005 <shubhammalviya2005@gmail.com>
2026-02-28 19:51:30 +0530

    create Contact table in postgres database

---
+++ schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS "Contact" (
+  id SERIAL PRIMARY KEY,
+  "phoneNumber" TEXT,
+  email TEXT,
+  "linkedId" INTEGER,
+  "linkPrecedence" TEXT NOT NULL,
+  "createdAt" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  "updatedAt" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  "deletedAt" TIMESTAMP
+);
+
+CREATE INDEX IF NOT EXISTS idx_email ON "Contact"(email);
+CREATE INDEX IF NOT EXISTS idx_phone ON "Contact"("phoneNumber");
+CREATE INDEX IF NOT EXISTS idx_linkedId ON "Contact"("linkedId");
